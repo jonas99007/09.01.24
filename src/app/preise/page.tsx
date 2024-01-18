475b68393927ab7ba40187428ef6e06b2c0e13c8
jonas99007 <jonasfesehazion@yahoo.de>
2024-01-18 19:26:46 +0100

    Preise: Untere Leiste

--- src/app/preise/page.tsx
+++ src/app/preise/page.tsx
@@ -1,41 +1,51 @@
+const navigation = [
+
+{ name: 'Sprechen sie mit uns', href: 'kontakt' },
+]
 export default function Example() {
     return (
-      <div className="bg-white py-24 sm:py-64">
-        <div className="mx-auto max-w-7xl px-6 lg:px8">
-          <div className="-mx-6 grid grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-3">
-            <div className="bg-gray-700/5 p-8 sm:p-10">
-              <p>Zeiten</p>
+        
+      <div className="bg-white py-24 sm:py-32 h-screen shadow-xl overflow-hidden bg-cover bg-center" style={{ backgroundImage: 'url("tennis2.jpg")' }}>
+        
+       
+        <div className="mx-auto max-w-7xl px-6 lg:px8 w-3/4">
+          <div className="-mx-6 grid grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-3 ">
+            <div className="bg-gray-300/90  p-8 sm:p-10">
+              <p className=" font-semibold">Zeiten</p>
             </div>
-
-            <div className="bg-gray-700/5 p-8 sm:p-10">
+            <div className="font-semibold bg-gray-300/90 p-8 sm:p-10 ">
               <p>Preis pro Stunde </p> <p>Winter / Sommer</p>
             </div>
-
-            <div className="bg-gray-700/5 p-8 sm:p-10">
-              <p>ABO-Preise Winter Saison</p> <p> (ABO-Preise Sommer auf Anfrage;</p>  <p>Buchung sofort möglich)</p>
+            <div className="font-semibold bg-gray-300/90 p-8 sm:p-10">
+              <p>ABO-Preise Winter Saison</p> <p> (Preise Sommer auf Anfrage;</p>  <p>Buchung sofort möglich)</p>
             </div>
-            <div className="bg-gray-700/5 p-8 sm:p-10">
+            <div className=" bg-gray-300/90 p-8 sm:p-10">
               <p> Montag bis Freitag</p> <p>09:00-15:00 </p> <p>15:00-22:00</p>
             </div>
-            <div className="bg-gray-700/5 p-8 sm:p-10">
-              <p className="text-neutral-100"> füller</p><p>19€ / 10€</p> <p> 26€ / 15€</p>
+            <div className="bg-gray-300/90 p-8 sm:p-10">
+              <p className="text-neutral-100 opacity-0"> füller</p><p>19€ / 10€</p> <p> 26€ / 15€</p>
             </div>
-            <div className="bg-gray-700/5 p-8 sm:p-10">
-            <p className="text-neutral-100"> füller</p><p>420€ </p> <p> 620€</p>
+            <div className="bg-gray-300/90 p-8 sm:p-10">
+            <p className="text-neutral-100 opacity-0"> füller</p><p>420€ </p> <p> 620€</p>
             </div>
-            <div className="bg-gray-700/5 p-8 sm:p-10">
+            <div className="bg-gray-300/90 p-8 sm:p-10">
               <p>Samstag & Sonntag </p> <p>09:00-15:00 </p> <p>15:00-22:00</p>
             </div>
-            <div className="bg-gray-700/5 p-8 sm:p-10">
-            <p className="text-neutral-100"> füller</p><p> 16€ / 10€</p> <p> 19€ / 15€</p>
+            <div className="bg-gray-300/90 p-8 sm:p-10">
+            <p className="text-neutral-100 opacity-0"> füller</p><p> 16€ / 10€</p> <p> 19€ / 15€</p>
             </div>
 
-            <div className="bg-gray-700/5 p-8 sm:p-10">
-            <p className="text-neutral-100"> füller</p><p>390€</p> <p> 520€</p>
+            <div className="bg-gray-300/90 p-8 sm:p-10">
+            <p className="text-neutral-100 opacity-0"> füller</p><p>390€</p> <p> 520€</p>
             </div>
-            
-            
+        
           </div>
+            
+          <div> <style> "text-neutral-100 opacity-0"</style> <p>...</p></div>
+        <div className="border-b border-gray-200 bg-gray-300/90 px-4 py-5 sm:px-6 sm:mx-0 sm:rounded-2xl">
+         <h1 className="text-neutral font-semibold leading-6 text-gray-900 ">Trainingsangebot : Einzel- oder Gruppenunterricht nach Absprache </h1>
+           <h2> Bespannungsservice im Haus</h2> <p> Haben Sie Sonderwünsche? Sprechen Sie uns an.</p>
+     </div>
         </div>
       </div>
     )
